c90764797d638e9575803a718afd1f4d5c64d370
Alex Rybak <alexrybak.pr@gmail.com>
2019-01-04 16:20:15 +0200

    added base styling; added page for not loggined users; moved all routes to the separate file

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ class App extends React.Component<{}, { loggedIn: boolean }> {
     super(props);
 
     this.state = {
-      loggedIn: true
+      loggedIn: false
     };
   }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import './reset.css';
+import './global.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
